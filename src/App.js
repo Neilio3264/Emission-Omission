9097c69f44d30ddcd29a25c8e4e7426adcc0dc33
Neilio3264 <neilrathod12@gmail.com>
2021-01-23 13:12:19 -0600

    Add API changes

--- src/App.js
+++ src/App.js
@@ -1,35 +1,78 @@
 import React from "react";
-import { GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps";
+
+// =============== IMPORTS FOR MAP ======================
+import { GoogleMap, useLoadScript } from "@react-google-maps/api";
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from "@reach/combobox";
+import "@reach/combobox/styles.css";
+// ======================================================
+
 import "./App.css";
-import Header from "./components/Header.js"
-import MainCard from "./components/MainCard.js"
+import Header from "./components/Header.js";
+import MainCard from "./components/MainCard.js";
 //import PageWrapper from "./components/PageWrapper.js"
 
-function Map() {
-  return (
-    <GoogleMap
-      defaultZoom={13}
-      defaultCenter={{ lat: 32.987205, lng: -96.748258 }}
-    />
-  );
-}
+// ============== BASIC SETTINGS FOR MAP ================
+const libraries = ["places"];
+const mapContainerStyle = {
+  width: "60vw",
+  height: "750px",
+};
+const center = {
+  lat: 32.987205,
+  lng: -96.748258,
+};
+// ======================================================
 
-const WrappedMap = withScriptjs(withGoogleMap(Map));
+export default function App() {
+  // ================= LOAD SCRIPTS FOR MAP ================
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAP_API,
+    libraries,
+  });
 
-function App() {
+  const mapRef = React.useRef();
+  const onMapLoad = React.useCallback((map) => {
+    mapRef.current = map;
+  }, []);
+
+  const panTo = React.useCallback(({ lat, lng }) => {
+    mapRef.current.panTo({ lat, lng });
+    mapRef.current.setZoom(13);
+  }, []);
+
+  if (loadError) return "Error";
+  if (!isLoaded) return "Loading...";
+  // =======================================================
+
   return (
     <body>
       <div class="page-wrapper">
         <Header></Header>
         <MainCard>
-            <div class="map-wrapper">
-            <WrappedMap
-              googleMapURL={`${process.env.REACT_APP_GOOGLE_MAP_URL}&key=${process.env.REACT_APP_GOOGLE_MAP_API}`}
-              loadingElement={<div style={{ height: `100%` }} />}
-              containerElement={<div style={{ height: `100%` }} />}
-              mapElement={<div style={{ height: `100%` }} />}
-            />
-          </div>  
+          {/* ============ MAP, SEARCH, And GEOLOCATION ============= */}
+          <Search panTo={panTo} /> {/* Can move to where you need */}
+          <div class="map-wrapper">
+            <Locate panTo={panTo} />
+            <GoogleMap
+              id="map"
+              mapContainerStyle={mapContainerStyle}
+              zoom={13}
+              center={center}
+              //options={options}
+              onLoad={onMapLoad}
+            ></GoogleMap>
+          </div>
+          {/* ======================================================= */}
         </MainCard>
         <footer></footer>
       </div>
@@ -37,4 +80,78 @@ function App() {
   );
 }
 
-export default App;
+// ================== EVERYTHING BELOW IS FOR MAP =========================
+
+function Locate({ panTo }) {
+  return (
+    <button
+      className="locate"
+      onClick={() => {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            panTo({
+              lat: position.coords.latitude,
+              lng: position.coords.longitude,
+            });
+          },
+          () => null
+        );
+      }}
+    >
+      <img src="/compass.svg" alt="compass" />
+    </button>
+  );
+}
+
+function Search({ panTo }) {
+  const {
+    ready,
+    value,
+    suggestions: { status, data },
+    setValue,
+    clearSuggestions,
+  } = usePlacesAutocomplete({
+    requestOptions: {
+      location: { lat: () => 32.987205, lng: () => -96.748258 },
+      radius: 100 * 1000, //meters
+    },
+  });
+
+  const handleInput = (e) => {
+    setValue(e.target.value);
+  };
+
+  const handleSelect = async (address) => {
+    setValue(address, false);
+    clearSuggestions();
+
+    try {
+      const results = await getGeocode({ address });
+      const { lat, lng } = await getLatLng(results[0]);
+      panTo({ lat, lng });
+    } catch (error) {
+      console.log("😱 Error: ", error);
+    }
+  };
+
+  return (
+    <div className="search">
+      <Combobox onSelect={handleSelect}>
+        <ComboboxInput
+          value={value}
+          onChange={handleInput}
+          disabled={!ready}
+          placeholder="Search your location"
+        />
+        <ComboboxPopover>
+          <ComboboxList>
+            {status === "OK" &&
+              data.map(({ id, description }) => (
+                <ComboboxOption key={id} value={description} />
+              ))}
+          </ComboboxList>
+        </ComboboxPopover>
+      </Combobox>
+    </div>
+  );
+}
