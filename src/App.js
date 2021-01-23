b95ae1158b7ecb65688c175e74cd31871f1423df
Neilio3264 <neilrathod12@gmail.com>
2021-01-23 15:25:09 -0600

    Format code

--- src/App.js
+++ src/App.js
@@ -1,95 +1,16 @@
 import React from "react";
 
-// =============== IMPORTS FOR MAP ======================
-<<<<<<< HEAD
-import { GoogleMap, useLoadScript, DirectionsService, DirectionsRenderer } from "@react-google-maps/api";
-import usePlacesAutocomplete, {
-  getGeocode,
-  getLatLng,
-} from "use-places-autocomplete";
-import {
-  Combobox,
-  ComboboxInput,
-  ComboboxPopover,
-  ComboboxList,
-  ComboboxOption,
-} from "@reach/combobox";
-import "@reach/combobox/styles.css";
-=======
-// import { GoogleMap, useLoadScript } from "@react-google-maps/api";
-// import usePlacesAutocomplete, {
-//   getGeocode,
-//   getLatLng,
-// } from "use-places-autocomplete";
-// import {
-//   Combobox,
-//   ComboboxInput,
-//   ComboboxPopover,
-//   ComboboxList,
-//   ComboboxOption,
-// } from "@reach/combobox";
-// import "@reach/combobox/styles.css";
->>>>>>> frontend
-// ======================================================
-
 import "./App.css";
 import Header from "./components/Header.js";
 import MainCard from "./components/MainCard.js";
-//import PageWrapper from "./components/PageWrapper.js"
-
-
-// ============== BASIC SETTINGS FOR MAP ================
-// const libraries = ["places"];
-// const mapContainerStyle = {
-//   width: "60vw",
-//   height: "750px",
-// };
-// const center = {
-//   lat: 32.987205,
-//   lng: -96.748258,
-// };
-// ======================================================
 
 export default function App() {
-  // ================= LOAD SCRIPTS FOR MAP ================
-  // const { isLoaded, loadError } = useLoadScript({
-  //   googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAP_API,
-  //   libraries,
-  // });
-
-  // const mapRef = React.useRef();
-  // const onMapLoad = React.useCallback((map) => {
-  //   mapRef.current = map;
-  // }, []);
-
-  // const panTo = React.useCallback(({ lat, lng }) => {
-  //   mapRef.current.panTo({ lat, lng });
-  //   mapRef.current.setZoom(13);
-  // }, []);
-
-  // if (loadError) return "Error";
-  // if (!isLoaded) return "Loading...";
-  // =======================================================
-
   return (
     <body>
       <div class="page-wrapper">
         <Header></Header>
         <MainCard>
-          {/* ============ MAP, SEARCH, And GEOLOCATION ============= */}
-          
-          <div class="map-wrapper">
-            
-            {/* <GoogleMap
-              id="map"
-              mapContainerStyle={mapContainerStyle}
-              zoom={13}
-              center={center}
-              //options={options}
-              onLoad={onMapLoad}
-            ></GoogleMap> */}
-          </div>
-          {/* ======================================================= */}
+          <div class="map-wrapper"></div>
         </MainCard>
         <footer></footer>
       </div>
@@ -98,331 +19,270 @@ export default function App() {
 }
 
 class Directions extends React.Component {
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
 
     this.state = {
       response: null,
-      travelMode: 'DRIVING',
-      origin: '',
-      destination: ''
-    }
+      travelMode: "DRIVING",
+      origin: "",
+      destination: "",
+    };
 
-    this.directionsCallback = this.directionsCallback.bind(this)
-    this.checkDriving = this.checkDriving.bind(this)
-    this.checkBicycling = this.checkBicycling.bind(this)
-    this.checkTransit = this.checkTransit.bind(this)
-    this.checkWalking = this.checkWalking.bind(this)
-    this.getOrigin = this.getOrigin.bind(this)
-    this.getDestination = this.getDestination.bind(this)
-    this.onClick = this.onClick.bind(this)
-    this.onMapClick = this.onMapClick.bind(this)
+    this.directionsCallback = this.directionsCallback.bind(this);
+    this.checkDriving = this.checkDriving.bind(this);
+    this.checkBicycling = this.checkBicycling.bind(this);
+    this.checkTransit = this.checkTransit.bind(this);
+    this.checkWalking = this.checkWalking.bind(this);
+    this.getOrigin = this.getOrigin.bind(this);
+    this.getDestination = this.getDestination.bind(this);
+    this.onClick = this.onClick.bind(this);
+    this.onMapClick = this.onMapClick.bind(this);
   }
 
-  directionsCallback (response) {
-    console.log(response)
+  directionsCallback(response) {
+    console.log(response);
 
     if (response !== null) {
-      if (response.status === 'OK') {
-        this.setState(
-          () => ({
-            response
-          })
-        )
+      if (response.status === "OK") {
+        this.setState(() => ({
+          response,
+        }));
       } else {
-        console.log('response: ', response)
+        console.log("response: ", response);
       }
     }
   }
 
-  checkDriving ({ target: { checked } }) {
+  checkDriving({ target: { checked } }) {
     checked &&
-      this.setState(
-        () => ({
-          travelMode: 'DRIVING'
-        })
-      )
+      this.setState(() => ({
+        travelMode: "DRIVING",
+      }));
   }
 
-  checkBicycling ({ target: { checked } }) {
+  checkBicycling({ target: { checked } }) {
     checked &&
-      this.setState(
-        () => ({
-          travelMode: 'BICYCLING'
-        })
-      )
+      this.setState(() => ({
+        travelMode: "BICYCLING",
+      }));
   }
 
-  checkTransit ({ target: { checked } }) {
+  checkTransit({ target: { checked } }) {
     checked &&
-      this.setState(
-        () => ({
-          travelMode: 'TRANSIT'
-        })
-      )
+      this.setState(() => ({
+        travelMode: "TRANSIT",
+      }));
   }
 
-  checkWalking ({ target: { checked } }) {
+  checkWalking({ target: { checked } }) {
     checked &&
-      this.setState(
-        () => ({
-          travelMode: 'WALKING'
-        })
-      )
+      this.setState(() => ({
+        travelMode: "WALKING",
+      }));
   }
 
-  getOrigin (ref) {
-    this.origin = ref
+  getOrigin(ref) {
+    this.origin = ref;
   }
 
-  getDestination (ref) {
-    this.destination = ref
+  getDestination(ref) {
+    this.destination = ref;
   }
 
-  onClick () {
-    if (this.origin.value !== '' && this.destination.value !== '') {
-      this.setState(
-        () => ({
-          origin: this.origin.value,
-          destination: this.destination.value
-        })
-      )
+  onClick() {
+    if (this.origin.value !== "" && this.destination.value !== "") {
+      this.setState(() => ({
+        origin: this.origin.value,
+        destination: this.destination.value,
+      }));
     }
   }
 
-  onMapClick (...args) {
-    console.log('onClick args: ', args)
+  onMapClick(...args) {
+    console.log("onClick args: ", args);
   }
 
-  render () {
+  render() {
     return (
-      <div className='map'>
-        <div className='map-settings'>
-          <hr className='mt-0 mb-3' />
+      <div className="map">
+        <div className="map-settings">
+          <hr className="mt-0 mb-3" />
 
-          <div className='row'>
-            <div className='col-md-6 col-lg-4'>
-              <div className='form-group'>
-                <label htmlFor='ORIGIN'>Origin</label>
+          <div className="row">
+            <div className="col-md-6 col-lg-4">
+              <div className="form-group">
+                <label htmlFor="ORIGIN">Origin</label>
                 <br />
-                <input id='ORIGIN' className='form-control' type='text' ref={this.getOrigin} />
+                <input
+                  id="ORIGIN"
+                  className="form-control"
+                  type="text"
+                  ref={this.getOrigin}
+                />
               </div>
             </div>
 
-            <div className='col-md-6 col-lg-4'>
-              <div className='form-group'>
-                <label htmlFor='DESTINATION'>Destination</label>
+            <div className="col-md-6 col-lg-4">
+              <div className="form-group">
+                <label htmlFor="DESTINATION">Destination</label>
                 <br />
-                <input id='DESTINATION' className='form-control' type='text' ref={this.getDestination} />
+                <input
+                  id="DESTINATION"
+                  className="form-control"
+                  type="text"
+                  ref={this.getDestination}
+                />
               </div>
             </div>
           </div>
 
-          <div className='d-flex flex-wrap'>
-            <div className='form-group custom-control custom-radio mr-4'>
+          <div className="d-flex flex-wrap">
+            <div className="form-group custom-control custom-radio mr-4">
               <input
-                id='DRIVING'
-                className='custom-control-input'
-                name='travelMode'
-                type='radio'
-                checked={this.state.travelMode === 'DRIVING'}
+                id="DRIVING"
+                className="custom-control-input"
+                name="travelMode"
+                type="radio"
+                checked={this.state.travelMode === "DRIVING"}
                 onChange={this.checkDriving}
               />
-              <label className='custom-control-label' htmlFor='DRIVING'>Driving</label>
+              <label className="custom-control-label" htmlFor="DRIVING">
+                Driving
+              </label>
             </div>
 
-            <div className='form-group custom-control custom-radio mr-4'>
+            <div className="form-group custom-control custom-radio mr-4">
               <input
-                id='BICYCLING'
-                className='custom-control-input'
-                name='travelMode'
-                type='radio'
-                checked={this.state.travelMode === 'BICYCLING'}
+                id="BICYCLING"
+                className="custom-control-input"
+                name="travelMode"
+                type="radio"
+                checked={this.state.travelMode === "BICYCLING"}
                 onChange={this.checkBicycling}
               />
-              <label className='custom-control-label' htmlFor='BICYCLING'>Bicycling</label>
+              <label className="custom-control-label" htmlFor="BICYCLING">
+                Bicycling
+              </label>
             </div>
 
-            <div className='form-group custom-control custom-radio mr-4'>
+            <div className="form-group custom-control custom-radio mr-4">
               <input
-                id='TRANSIT'
-                className='custom-control-input'
-                name='travelMode'
-                type='radio'
-                checked={this.state.travelMode === 'TRANSIT'}
+                id="TRANSIT"
+                className="custom-control-input"
+                name="travelMode"
+                type="radio"
+                checked={this.state.travelMode === "TRANSIT"}
                 onChange={this.checkTransit}
               />
-              <label className='custom-control-label' htmlFor='TRANSIT'>Transit</label>
+              <label className="custom-control-label" htmlFor="TRANSIT">
+                Transit
+              </label>
             </div>
 
-            <div className='form-group custom-control custom-radio mr-4'>
+            <div className="form-group custom-control custom-radio mr-4">
               <input
-                id='WALKING'
-                className='custom-control-input'
-                name='travelMode'
-                type='radio'
-                checked={this.state.travelMode === 'WALKING'}
+                id="WALKING"
+                className="custom-control-input"
+                name="travelMode"
+                type="radio"
+                checked={this.state.travelMode === "WALKING"}
                 onChange={this.checkWalking}
               />
-              <label className='custom-control-label' htmlFor='WALKING'>Walking</label>
+              <label className="custom-control-label" htmlFor="WALKING">
+                Walking
+              </label>
             </div>
           </div>
 
-          <button className='btn btn-primary' type='button' onClick={this.onClick}>
+          <button
+            className="btn btn-primary"
+            type="button"
+            onClick={this.onClick}
+          >
             Build Route
           </button>
         </div>
 
-        <div className='map-container'>
+        <div className="map-container">
           <GoogleMap
             // required
-            id='direction-example'
+            id="direction-example"
             // required
             mapContainerStyle={{
-              height: '400px',
-              width: '100%'
+              height: "400px",
+              width: "100%",
             }}
             // required
             zoom={2}
             // required
             center={{
               lat: 0,
-              lng: -180
+              lng: -180,
             }}
             // optional
             onClick={this.onMapClick}
             // optional
-            onLoad={map => {
-              console.log('DirectionsRenderer onLoad map: ', map)
+            onLoad={(map) => {
+              console.log("DirectionsRenderer onLoad map: ", map);
             }}
             // optional
-            onUnmount={map => {
-              console.log('DirectionsRenderer onUnmount map: ', map)
+            onUnmount={(map) => {
+              console.log("DirectionsRenderer onUnmount map: ", map);
             }}
           >
-            {
-              (
-                this.state.destination !== '' &&
-                this.state.origin !== ''
-              ) && (
-                <DirectionsService
-                  // required
-                  options={{ 
-                    destination: this.state.destination,
-                    origin: this.state.origin,
-                    travelMode: this.state.travelMode
-                  }}
-                  // required
-                  callback={this.directionsCallback}
-                  // optional
-                  onLoad={directionsService => {
-                    console.log('DirectionsService onLoad directionsService: ', directionsService)
-                  }}
-                  // optional
-                  onUnmount={directionsService => {
-                    console.log('DirectionsService onUnmount directionsService: ', directionsService)
-                  }}
-                />
-              )
-            }
+            {this.state.destination !== "" && this.state.origin !== "" && (
+              <DirectionsService
+                // required
+                options={{
+                  destination: this.state.destination,
+                  origin: this.state.origin,
+                  travelMode: this.state.travelMode,
+                }}
+                // required
+                callback={this.directionsCallback}
+                // optional
+                onLoad={(directionsService) => {
+                  console.log(
+                    "DirectionsService onLoad directionsService: ",
+                    directionsService
+                  );
+                }}
+                // optional
+                onUnmount={(directionsService) => {
+                  console.log(
+                    "DirectionsService onUnmount directionsService: ",
+                    directionsService
+                  );
+                }}
+              />
+            )}
 
-            {
-              this.state.response !== null && (
-                <DirectionsRenderer
-                  // required
-                  options={{
-                    directions: this.state.response
-                  }}
-                  // optional
-                  onLoad={directionsRenderer => {
-                    console.log('DirectionsRenderer onLoad directionsRenderer: ', directionsRenderer)
-                  }}
-                  // optional
-                  onUnmount={directionsRenderer => {
-                    console.log('DirectionsRenderer onUnmount directionsRenderer: ', directionsRenderer)
-                  }}
-                />
-              )
-            }
+            {this.state.response !== null && (
+              <DirectionsRenderer
+                // required
+                options={{
+                  directions: this.state.response,
+                }}
+                // optional
+                onLoad={(directionsRenderer) => {
+                  console.log(
+                    "DirectionsRenderer onLoad directionsRenderer: ",
+                    directionsRenderer
+                  );
+                }}
+                // optional
+                onUnmount={(directionsRenderer) => {
+                  console.log(
+                    "DirectionsRenderer onUnmount directionsRenderer: ",
+                    directionsRenderer
+                  );
+                }}
+              />
+            )}
           </GoogleMap>
         </div>
       </div>
-    )
+    );
   }
 }
-
-// ================== EVERYTHING BELOW IS FOR MAP =========================
-
-// function Locate({ panTo }) {
-//   return (
-//     <button
-//       className="locate"
-//       onClick={() => {
-//         navigator.geolocation.getCurrentPosition(
-//           (position) => {
-//             panTo({
-//               lat: position.coords.latitude,
-//               lng: position.coords.longitude,
-//             });
-//           },
-//           () => null
-//         );
-//       }}
-//     >
-//       <img src="/compass.svg" alt="compass" />
-//     </button>
-//   );
-// }
-
-// // function Search({ panTo }) {
-//   const {
-//     ready,
-//     value,
-//     suggestions: { status, data },
-//     setValue,
-//     clearSuggestions,
-//   } = usePlacesAutocomplete({
-//     requestOptions: {
-//       location: { lat: () => 32.987205, lng: () => -96.748258 },
-//       radius: 100 * 1000, //meters
-//     },
-//   });
-
-//   const handleInput = (e) => {
-//     setValue(e.target.value);
-//   };
-
-//   const handleSelect = async (address) => {
-//     setValue(address, false);
-//     clearSuggestions();
-
-//     try {
-//       const results = await getGeocode({ address });
-//       const { lat, lng } = await getLatLng(results[0]);
-//       panTo({ lat, lng });
-//     } catch (error) {
-//       console.log("😱 Error: ", error);
-//     }
-//   };
-
-//   return (
-//     <div className="search">
-//       <Combobox onSelect={handleSelect}>
-//         <ComboboxInput
-//           value={value}
-//           onChange={handleInput}
-//           disabled={!ready}
-//           placeholder="Search your location"
-//         />
-//         <ComboboxPopover>
-//           <ComboboxList>
-//             {status === "OK" &&
-//               data.map(({ id, description }) => (
-//                 <ComboboxOption key={id} value={description} />
-//               ))}
-//           </ComboboxList>
-//         </ComboboxPopover>
-//       </Combobox>
-//     </div>
-//   );
-// }
--- src/components/MainCard.js
+++ src/components/MainCard.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 // =============== IMPORTS FOR MAP ======================
 import { GoogleMap, useLoadScript } from "@react-google-maps/api";
 import usePlacesAutocomplete, {
@@ -14,9 +14,7 @@ import {
 } from "@reach/combobox";
 import "@reach/combobox/styles.css";
 // ======================================================
-import './MainCard.css'
-// import Locate from "./GoogleMaps/Locate.js";
-// import Search from "./GoogleMaps/Search.js";
+import "./MainCard.css";
 
 const libraries = ["places"];
 const mapContainerStyle = {
@@ -29,112 +27,110 @@ const center = {
 };
 
 export default function MainCard() {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAP_API,
+    libraries,
+  });
 
-    const { isLoaded, loadError } = useLoadScript({
-        googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAP_API,
-        libraries,
-      });
-    
-      const mapRef = React.useRef();
-      const onMapLoad = React.useCallback((map) => {
-        mapRef.current = map;
-      }, []);
-    
-      const panTo = React.useCallback(({ lat, lng }) => {
-        mapRef.current.panTo({ lat, lng });
-        mapRef.current.setZoom(13);
-      }, []);
-    
-      if (loadError) return "Error";
-      if (!isLoaded) return "Loading...";
+  const mapRef = React.useRef();
+  const onMapLoad = React.useCallback((map) => {
+    mapRef.current = map;
+  }, []);
 
-    return (
-        <div class = 'main-card'>
-            <Search panTo={panTo} /> {/* Can move to where you need */}
-            <Locate panTo={panTo} />
-            <GoogleMap
-              id="map"
-              mapContainerStyle={mapContainerStyle}
-              zoom={13}
-              center={center}
-              //options={options}
-              onLoad={onMapLoad}
-            ></GoogleMap>
-        </div>
-    )
+  const panTo = React.useCallback(({ lat, lng }) => {
+    mapRef.current.panTo({ lat, lng });
+    mapRef.current.setZoom(13);
+  }, []);
+
+  if (loadError) return "Error";
+  if (!isLoaded) return "Loading...";
+
+  return (
+    <div class="main-card">
+      <Search panTo={panTo} /> {/* Can move to where you need */}
+      <Locate panTo={panTo} />
+      <GoogleMap
+        id="map"
+        mapContainerStyle={mapContainerStyle}
+        zoom={13}
+        center={center}
+        //options={options}
+        onLoad={onMapLoad}
+      ></GoogleMap>
+    </div>
+  );
 }
 
-
 function Locate({ panTo }) {
-    return (
+  return (
     <button
-        className="locate"
-        onClick={() => {
+      className="locate"
+      onClick={() => {
         navigator.geolocation.getCurrentPosition(
-            (position) => {
+          (position) => {
             panTo({
-                lat: position.coords.latitude,
-                lng: position.coords.longitude,
+              lat: position.coords.latitude,
+              lng: position.coords.longitude,
             });
-            },
-            () => null
+          },
+          () => null
         );
-        }}
+      }}
     >
-        <img src="/compass.svg" alt="compass" />
+      <img src="/compass.svg" alt="compass" />
     </button>
-    );
+  );
 }
 
 function Search({ panTo }) {
-    const {
+  const {
     ready,
     value,
     suggestions: { status, data },
     setValue,
     clearSuggestions,
-    } = usePlacesAutocomplete({
+  } = usePlacesAutocomplete({
     requestOptions: {
-        location: { lat: () => 32.987205, lng: () => -96.748258 },
-        radius: 100 * 1000, //meters
+      location: { lat: () => 32.987205, lng: () => -96.748258 },
+      radius: 100 * 1000, //meters
     },
-    });
+  });
 
-    const handleInput = (e) => {
+  const handleInput = (e) => {
     setValue(e.target.value);
-    };
+  };
 
-    const handleSelect = async (address) => {
+  const handleSelect = async (address) => {
     setValue(address, false);
     clearSuggestions();
 
     try {
-        const results = await getGeocode({ address });
-        const { lat, lng } = await getLatLng(results[0]);
-        panTo({ lat, lng });
+      const results = await getGeocode({ address });
+      const { lat, lng } = await getLatLng(results[0]);
+      panTo({ lat, lng });
     } catch (error) {
-        console.log("😱 Error: ", error);
+      console.log("😱 Error: ", error);
     }
-    };
+  };
 
-    return (
+  return (
     <div className="search">
-        <Combobox onSelect={handleSelect}>
+      <Combobox onSelect={handleSelect}>
         <ComboboxInput
-            value={value}
-            onChange={handleInput}
-            disabled={!ready}
-            placeholder="Search your location"
+          value={value}
+          onChange={handleInput}
+          disabled={!ready}
+          placeholder="Search your location"
         />
         <ComboboxPopover>
-            <ComboboxList>
+          <ComboboxList>
             {status === "OK" &&
-                data.map(({ id, description }) => (
+              data.map(({ id, description }) => (
                 <ComboboxOption key={id} value={description} />
-                ))}
-            </ComboboxList>
+              ))}
+          </ComboboxList>
         </ComboboxPopover>
-        </Combobox>
+      </Combobox>
     </div>
-    );
+  );
 }
